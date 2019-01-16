b0a1442f96a490d3c9b40c11462be2a3e7e656a0
Reyaz Ahmad <reyazoct@gmail.com>
2019-01-17 00:25:31 +0530

    Add: Pangram with mixed case and punctuation

--- pangram.js
+++ pangram.js
@@ -1,7 +1,7 @@
 export function isPangram(string) {
     let charSet = new Set();
     for (let iter = 0; iter < string.length; iter++) {
-        if (string[iter].match(/[a-z]/))
+        if (string[iter].match(/[a-zA-Z]/))
             charSet.add(string[iter]);
     }
     if (charSet.size == 26)
--- pangram.spec.js
+++ pangram.spec.js
@@ -28,4 +28,8 @@ describe('Pangram', () => {
     test('Pangram with numbers', () => {
         expect(isPangram('7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog')).toBe(false);
     });
+
+    test('Pangram with mixed case and punctuation', () => {
+        expect(isPangram('"Five quacking Zephyrs jolt my wax bed."')).toBe(true);
+    });
 });
