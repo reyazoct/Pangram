0bccf0862892d6e789d849c53b333d7b2c905d1b
Reyaz Ahmad <reyazoct@gmail.com>
2019-01-17 00:11:42 +0530

    Add: Lowercase Pangram with whitespaces and a missing character 'x'

--- pangram.spec.js
+++ pangram.spec.js
@@ -12,4 +12,8 @@ describe('Pangram', () => {
     test('Lowercase Pangram with whitespaces', () => {
         expect(isPangram('the quick brown fox jumps over the lazy dog')).toBe(true);
     });
+
+    test("Lowercase Pangram with whitespaces and a missing character 'x'", () => {
+        expect(isPangram('a quick movement of the enemy will jeopardize five gunboats')).toBe(false);
+    });
 });
